d9ae9d37731f465342f935ccfc2c1604eb714c40
japjeet <elemental@localhost.localdomain>
2023-01-22 14:15:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar1 from "./components/Navbar1/Navbar1";
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import "./App.css";
-import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonial from "./components/Testimonials/Testimonial";
